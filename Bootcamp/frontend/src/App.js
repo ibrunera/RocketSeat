6b7b7fd9b855bb23acd6a220b0d713ff65b6536c
Bruno T <bteixeira010@gmail.com>
2020-11-18 02:00:19 -0300

    imgs e css

--- Bootcamp/frontend/src/App.js
+++ Bootcamp/frontend/src/App.js
@@ -1,18 +1,24 @@
 import React, {useState} from 'react';
 
+import './App.css';
+import bckgImg from './assets/dog.jpg';
+
 import Header from './components/Header';
 
 function App(){
-  const [projects,setProjects] = useState([])
+  const [projects,setProjects] = useState([]);
 
   function handleAddProject(){
-    setProjects([...projects, `Projeto ${Date.now()}`])
-    console.log(projects)
-  }
+    setProjects([...projects, `Projeto ${Date.now()}`]);
+    console.log(projects);
+  };
 
   return (
     <>
       <Header title="Projects"/>
+
+      <img width={300} src={bckgImg} />
+
       <ul>
           {projects.map(project=><li key={project}>{project}</li>)}
       </ul>
