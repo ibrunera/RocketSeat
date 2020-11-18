2e0b9122892866969111ace92ca3b876d2642f8c
Bruno T <bteixeira010@gmail.com>
2020-11-18 01:31:58 -0300

    Estado e imutabilidade

--- Bootcamp/frontend/src/App.js
+++ Bootcamp/frontend/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './components/Header';
 
 function App(){
+  const [projects,setProjects] = useState([])
+
+  function handleAddProject(){
+    setProjects([...projects, `Projeto ${Date.now()}`])
+    console.log(projects)
+  }
+
   return (
     <>
       <Header title="Projects"/>
+      <ul>
+          {projects.map(project=><li key={project}>{project}</li>)}
+      </ul>
+
+      <button
+        type="button"
+        onClick={handleAddProject}>
+        Adicionar Projeto
+      </button>
     </>
   );
 }
