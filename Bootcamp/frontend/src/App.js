a3c95813c045c8d98417537a7f23acf9b0358824
Bruno T <bteixeira010@gmail.com>
2020-11-18 00:16:45 -0300

    ?

--- Bootcamp/frontend/src/App.js
+++ Bootcamp/frontend/src/App.js
@@ -5,19 +5,7 @@ import Header from './components/Header';
 function App(){
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Projects"/>
     </>
   );
 }
