49d260f0b1a852381844ec8c6e654817f02bb571
Bruno T <bteixeira010@gmail.com>
2020-11-23 23:31:12 -0300

    conectando com api

--- Bootcamp/mobile/src/index.js
+++ Bootcamp/mobile/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import {View, StyleSheet, StatusBar, Text} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {SafeAreaView,FlatList, StyleSheet, StatusBar, Text} from 'react-native'
+import api from './services/api'
+
 
 export default function App(){
+  const [projects, setProjects] = useState([])
+
+  useEffect(()=>{
+    api.get('projects').then(response=>{
+      setProjects(response.data)
+    })
+  },[])
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project=>project.id}
+          renderItem={({item:project})=>(
+            <Text style={styles.title}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   )
 }
@@ -16,12 +32,9 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     backgroundColor:'#7159c1',
-    alignItems:'center',
-    justifyContent:'center',
   },
   title: {
     color:'#fff',
     fontSize:32,
-    fontWeight:'bold'
   }
 })
