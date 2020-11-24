dd54578bb0d80fc6aac730a0947aaf902e4be5fd
Bruno T <bteixeira010@gmail.com>
2020-11-23 23:50:50 -0300

    listando projetos

--- Bootcamp/mobile/src/index.js
+++ Bootcamp/mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {SafeAreaView,FlatList, StyleSheet, StatusBar, Text} from 'react-native'
+import {SafeAreaView,FlatList, StyleSheet, StatusBar, Text, TouchableOpacity} from 'react-native'
 import api from './services/api'
 
 
@@ -12,6 +12,16 @@ export default function App(){
     })
   },[])
 
+  async function handleAddProject(){
+    const response = await api.post('projects',{
+      title:`Projeto ${Date.now()}`,
+      owner:'Peter P.'
+    })
+    const project = response.data
+
+    setProjects([...projects,project])
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
@@ -23,6 +33,13 @@ export default function App(){
             <Text style={styles.title}>{project.title}</Text>
           )}
         />
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.btn}
+          onPress={handleAddProject}
+        > 
+          <Text style={styles.btnText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   )
@@ -30,11 +47,24 @@ export default function App(){
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
     backgroundColor:'#7159c1',
+    flex:1,
   },
   title: {
     color:'#fff',
     fontSize:32,
+  },
+  btn:{
+    alignItems:'center',
+    backgroundColor:'#fff',
+    borderRadius:5,
+    height:50,
+    justifyContent:'center',
+    margin:20,
+  },
+  btnText:{
+    color:'#7159c1',
+    fontSize:20,
+    fontWeight:'bold',
   }
 })
